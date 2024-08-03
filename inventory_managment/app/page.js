3b2d43908855ef4af93d65d8b64d3dd98c4d2807
Jad Alriyabi <71855107+Jadalriyabi@users.noreply.github.com>
2024-08-03 13:03:56 -0700

    second push

--- inventory_managment/app/page.js
+++ inventory_managment/app/page.js
@@ -4,8 +4,8 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
-import { Box, Typography } from "@mui/material";
-import { collection, query, getDocs } from "firebase/firestore";
+import { Box, Modal, Typography } from "@mui/material";
+import { collection, query, getDocs, getDoc } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
@@ -26,19 +26,55 @@ export default function Home() {
     console.log(inventoryList);
   };
 
+  const addItem =  async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      await setDoc(docRef, {quantity: quantity - 1})
+    }
+
+    await updateInventory()
+  }
+
+  const removeItem =  async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+      } else {
+        await setDoc(docRef, {quantity: 1})
+      }
+
+    await updateInventory()
+  }
+
+
   useEffect(() => {
     updateInventory();
   }, []);
 
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   return (
-    <Box>
-      <Typography variant="h1">Inventory Management</Typography>
-      {inventory.map((item, index) => (
-        <Box key={index}>
-          <Typography>{item.name}</Typography>
-          <Typography>{item.count}</Typography>
+    <Box 
+    width = "100vw" 
+    height = "100vh" 
+    display="flex" 
+    justifyContent= "center" 
+    alignItems="center" 
+    gap={2}
+    >
+      <Modal open={open} onClose={handleClose}>
+        <Box position="absolute">
+          
         </Box>
-      ))}
+      </Modal>
+      <Typography variant="h1">Inventory Management</Typography>
+      
     </Box>
-  );
+  )
 }
